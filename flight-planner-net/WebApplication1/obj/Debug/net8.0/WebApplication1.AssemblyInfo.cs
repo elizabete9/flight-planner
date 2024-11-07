91ee3367d276b8966c4dc9d7430e7f4deb3d73d1
elizabete9 <elizabetezveja32@gmail.com>
2024-11-07 12:33:54 +0200

    done

--- flight-planner-net/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ flight-planner-net/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f494954713cc6f75c3cd0a6d426bc6a4fbd090f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de673415574503990674cec1a45218f08f5e3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
